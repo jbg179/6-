2cf0da084cc0ef5b75215a46ddfef10aacaa571c
jbg179 <jbg179@naver.com>
2022-12-07 01:36:25 +0900

    6-11

---
+++ 6-11/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class SystemrExThrowApp
+{
+    public static void Exp(int ptr)
+    {
+        if (ptr == 0)
+            throw new NullReferenceException();
+    }
+    public static void Main()
+    {
+        int i = 0;
+        Exp(i);
+    }
+}
