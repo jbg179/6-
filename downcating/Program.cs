57c81770c65a5c7b8909783ac2119ad283bfb9e5
jbg179 <jbg179@naver.com>
2022-12-07 01:50:31 +0900

    down

---
+++ downcating/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+namespace Thread_parameter_passing_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(threadFunc);
+            t.Start(10);
+        }
+        static void threadFunc(object initialValue)
+        {
+            int intValue = (int)initialValue;
+            Console.WriteLine(intValue);
+        }
+    }
+}
